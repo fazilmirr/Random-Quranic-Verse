b0e35cb8f8921655e8139c7ea11506a630ba7d32
Fazil Fayaz <145839303+fazilmirr@users.noreply.github.com>
2024-03-10 16:57:46 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="bg-danger text-white Height d-flex justify-content-center align-items-center">
       <h5 className="fs-1">Random Quran verse</h5>
-      <p className="fs-2 mt-3 mx-3">by Fazil Fayaz</p>
+      <p className="fs-2 mt-3 mx-3">by Fazil Fayaz Mir</p>
     </div>
   );
 };
